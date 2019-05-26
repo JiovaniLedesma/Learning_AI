a2601ec000aac6aadc038add4d8e28d81b3d1a80
Jiovani Ledesma Arredondo <jiovaniledesmaa@gmail.com>
2019-05-26 12:34:48 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# Current status: pre-alpha
+
+setup(name='learningAI',
+      version='0.1.0',
+      description='Python library for learning about AI',
+      author='Jiovani Ledesma',
+      author_email='jiovaniledesmaa@gmail.com',
+      license = "MIT",
+      keywords=["AI","neural networks", "machine learning", "deep learning"],
+      url='https://github.com/JiovaniLedesma/Learning_AI',
+      packages=['learningAI'],
+      classifiers=[
+      "Development Status :: 2 - Pre-Alpha",
+      "Intended Audience :: Education",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3.6",
+      "Programming Language :: Python :: Implementation :: CPython",
+      ]
+      )
